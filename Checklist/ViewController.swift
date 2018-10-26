fd4e8c0c16460ca7311fe67e740078689175a3c9
mastersunny <sunnycse248@gamil.com>
2018-10-26 21:42:28 +0600

    First commit

--- Checklist/ViewController.swift
+++ Checklist/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            if(indexPath.row == 0){
+                label.text = "Run a marathon"
+            }else{
+                label.text =  "Sleep"
+            }
+            
+        }
+        
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath){
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            }else{
+                cell.accessoryType = .none
+            }
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
 
 
